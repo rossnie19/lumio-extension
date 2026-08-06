738d45204d4422bf2cb36a262fa24055ec7db996
rossnie19 <rossnie19@gmail.com>
2026-08-06 18:07:38 +0800

    shared/storage.js code

--- lumio-extension/shared/storage.js
+++ lumio-extension/shared/storage.js
@@ -1,7 +1,20 @@
-//this file acts as the bridge between the extension and chrome database (chrome.storage.local)
+//this file is the bridge between the extension and chrome database (chrome.storage.local)
 
 //get value from storage
 export async function getData(key) {
     const result = await chrome.storage.local.get(key);
     return result[key];
 }
+
+//set value in storage
+export async function setData(key, value) {
+    await chrome.storage.local.set({ [key]: value });
+}
+
+//add a usage log entry (used by backrgound worker)
+export async function logUsageEvent(domain) {
+    const logs = await getData("usageLogs") || [];
+    logs.push({ domain, timestamp: Date.now() });
+    await setData("usageLogs", logs);
+}
+
